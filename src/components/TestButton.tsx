9c7464150eac2532f8ce5d18a2bd549d429b14a1
Melissa Rose <123melissa321@gmail.com>
2022-10-19 13:59:46 +0100

    Push all edits before workspace deletion

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,5 +1,14 @@
 function TestButton(): JSX.Element {
-  return <button>Make me do something when I'm clicked...</button>;
+  return (
+    <button
+      onClick={() => {
+        console.log("stan Jungkook");
+        window.open();
+      }}
+    >
+      Make me do something when I'm clicked...
+    </button>
+  );
 }
 
 export default TestButton;
